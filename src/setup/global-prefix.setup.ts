197efee6a1224c42b1c935e1d701f0a0de326ba2
vitaliiprysiazhniuk <vetastektonik1@gmail.com>
2025-03-31 11:11:15 -0700

    fix: prefix for swagger

--- src/setup/global-prefix.setup.ts
+++ src/setup/global-prefix.setup.ts
@@ -1,8 +1,8 @@
 import { INestApplication } from '@nestjs/common';
 
-export const GLOBAL_PREFIX = 'api';
+export const GLOBAL_PREFIX = process.env.GLOBAL_PREFIX || 'api';
 
 export function globalPrefixSetup(app: INestApplication) {
   //специальный метод, который добавляет ко всем маршрутам /GLOBAL_PREFIX
-  app.setGlobalPrefix(process.env.GLOBAL_PREFIX || GLOBAL_PREFIX);
+  app.setGlobalPrefix(GLOBAL_PREFIX);
 }
--- src/setup/swagger.setup.ts
+++ src/setup/swagger.setup.ts
@@ -1,15 +1,16 @@
 import { INestApplication } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { GLOBAL_PREFIX } from './global-prefix.setup';
 
 export function swaggerSetup(app: INestApplication) {
   const config = new DocumentBuilder()
     .setTitle('BLOGGER API')
-    // .addBearerAuth()
+    .addBearerAuth()
     .setVersion('1.0')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document, {
+  SwaggerModule.setup(GLOBAL_PREFIX, app, document, {
     customSiteTitle: 'Blogger Swagger',
   });
 }
